26facf49c48ea3cc571800200a8a11d9331aa72a
MedetHasanUgurlu <mdt.hsn.76@hotmail.com>
2023-06-20 14:54:16 +0300

    medron#2

---
+++ medron/src/main/java/com/medet/medron/MedronApplication.java
@@ -0,0 +1,13 @@
+package com.medet.medron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedronApplication.class, args);
+	}
+
+}
